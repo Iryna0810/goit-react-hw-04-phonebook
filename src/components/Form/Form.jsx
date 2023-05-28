97c16f14ccc834e3df92ab8b950174ce4d9c246b
Rew <irinavakulenko2000@gmail.com>
2023-05-28 13:31:10 +0300

    111

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,59 +1,82 @@
-import React from 'react';
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 import { FormWrapper, Button, StyledInput } from "components/styled";
-const { Component } = require("react");
+import styled from "styled-components";
 
 
 
-export class Form extends Component {
 
-    state = {
-    name: '',
-    number: '',
-    }
+export const Form = ({onSubmit}) => {
 
-    handleInputChange = event => {
-    const { name, value } = event.currentTarget;
-      this.setState({ [name]: value });
-      
-  }
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+
+    // state = {
+    // name: '',
+    // number: '',
+    // }
+
+  //   handleInputChange = event => {
+  //   const { name, value } = event.currentTarget;
+  //     setName({ value });
+  //     setNumber({ value });      
+  // }
     
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
       e.preventDefault();
-      this.props.onSubmit(this.state);
-      this.reset();
+      // onSubmit(this.state);
+      reset();
+  }
+  
+    const handleInputChange = event => {
+    const { value, name } = event.target;
 
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      
+      case 'number':
+        setNumber(value);
+        break;
+      
+      default: return;
+    }
+  };
+
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+      // this.setState({
+      //   name: '',
+      //   number: '',
+      // })
     }
 
-    reset = () => {
-      this.setState({
-        name: '',
-        number: '',
-      })
-    }
 
-    render() {
-      return <FormWrapper action="" onSubmit={this.handleSubmit}>
+      return <FormWrapper action="" onSubmit={handleSubmit}>
         <label htmlFor="">Name
         <StyledInput
         type="text"
         name="name"
-        value={this.state.name}
+        value={name}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        onChange={this.handleInputChange}
+        onChange={handleInputChange}
         StyledInput/>
         </label>
         <label htmlFor="">Number
           <StyledInput
             type="tel"
             name="number"
-            value={this.state.number}
+            value={number}
   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            onChange={this.handleInputChange}
+            onChange={handleInputChange}
             StyledInput />
           </label>
          
@@ -62,6 +85,6 @@ export class Form extends Component {
        
     }
 
-}
+
 
 Form.propTypes = { onSubmit: PropTypes.func.isRequired};
